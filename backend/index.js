f576d087bd45edd2f8746e11451320b83cfefbcf
Tarun Kumar Sao <tarun.k.sao@gmail.com>
2023-01-18 21:52:06 +0530

    Added Backend for Products route

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { dbConnect } = require( './configs/db' );
+const productRoute = require('./routes/products.route');
+
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(cors());
+app.use('/products',productRoute);
+
+app.get('/', (req,res) => {
+    res.send('Welcome to Masai Furniture House');
+});
+
+app.listen(PORT, async () => {
+    try {
+        await dbConnect;
+        console.log('Connected to the DB');
+    } catch (e) {
+        console.log('Error occured while connecting to the DB');
+        console.log(e);
+    }
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
