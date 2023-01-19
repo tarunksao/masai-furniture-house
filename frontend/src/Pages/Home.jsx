fd46f505e2110228e9cb59d7fd2438858be8b264
dilippolynomial <dilip.singh@polynomial.ai>
2023-01-19 10:25:00 +0530

    home

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,11 +1,51 @@
 import React from 'react'
 import { Box, Heading } from '@chakra-ui/react';
-
+// import Carousel from '../components/carousel/carousel';
+// import ImageGallery from '../components/imageGallery/imageGallery';
+// import SalesButtons from '../components/salesButtons/salesButtons';
+// import Slider from '../components/slider/slider';
+// import GoodToKnowCards from '../components/goodToKnowCards/goodToKnowCards';
 const Home = () => {
   return (
+    <>
     <Box my={20} color='darkolivegreen'>
       <Heading textAlign='center'> MFH - Masai Furniture House</Heading>
     </Box>
+
+
+
+
+
+   
+      <ImageGallery />
+
+      <Carousel
+        condition={{ property: 'SalePrice', operator: '>', value: 0 }}
+      />
+
+      <SalesButtons />
+
+      <Slider />
+
+      <Carousel
+        condition={{ property: 'LowerPrice', operator: '==', value: true }}
+      />
+
+      <GoodToKnowCards />
+
+      <Slider />
+    </>
+  );
+};
+
+export default Home;
+
+
+
+
+
+
+</>
   )
 }
 
